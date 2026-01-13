0a0a146a0b3d8301b7d23c2a39e5342d95b9231f
rakeshreddy-hue <dudekondar.ug23.ec@nitp.ac.in>
2026-01-13 16:22:02 +0530

    Update baud_rate_generator.v

--- baud_rate_generator.v
+++ baud_rate_generator.v
@@ -11,26 +11,41 @@ module baud_ratewe_generator(input clk,reset,output tx_en,rx_en);
   always@(posedge clk)begin
     if(reset)begin
       tx_counter<=0;
+      tx_en=0;
+      rx_en=0;
     end
     
-    else if(tx_counter=tx_divisor-1)begin
+    else if(tx_counter==tx_divisor-1)begin
       tx_en=1;
       tx_counter=0;
     end
     else 
       begin
       tx_en=0;
-    tx_counter=tx_counter+1'b1;
+    tx_counter<=tx_counter+1'b1;
       end
-    if(rx_counter=rx_divisor-1)begin
+
+    
+ always@(posedge clk)begin
+    if(reset)begin
+      rx_counter<=0;
+     
+    end
+
+
+    
+    if(rx_counter==rx_divisor-1)begin
       rx_en=1;
       rx_counter=0;
     end
     else
       begin
         rx_en=0;
-        rx_counter=rx_counter+1'b1;
+        rx_counter<=rx_counter+1'b1;
       end
+ end
+
+    endmodule
     
     
   
